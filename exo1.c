911803b5aae7ba44e2ed7d3244736540636c9536
RAOYo <yoannrah12@outlook.com>
2025-08-19 17:15:56 +0300

    Update exo1.c

--- exo1.c
+++ exo1.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
+
 int main()
 {
     int a;
     printf("Veuillez entrer la valeur de A:");
     scanf("%d",&a);
+
     if (a>0)
-    {
         printf("le nombre est positif");
-    }
+    else if (a == 0)
+        printf("le nombre est nul");
     else
         printf("le nombre est négatif");
-
-        return (0);
+    return (0);
 }
